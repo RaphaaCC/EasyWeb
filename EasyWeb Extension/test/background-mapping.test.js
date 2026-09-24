f34c5900ed67dafa7e1545a9c61992e0cd2fb374
RaphaaCC <raphaelcherfemcarlini1@gmail.com>
2026-09-24 19:41:14 -0300

    Release v1.0.2 Beta with extension logo

--- EasyWeb Extension/test/background-mapping.test.js
+++ EasyWeb Extension/test/background-mapping.test.js
@@ -108,7 +108,7 @@ function loadBackground() {
     },
     chrome: {
       runtime: {
-        getManifest: () => ({ version_name: "v1.0.1 Beta" }),
+        getManifest: () => ({ version_name: "v1.0.2 Beta" }),
         onMessage: { addListener(listener) { messageListener = listener; } },
         onStartup: { addListener() {} }
       },
